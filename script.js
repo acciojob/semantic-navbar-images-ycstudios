c24cd842108e598383c9e6dad9593ddc456e0fed
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-22 10:16:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+ nav ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        nav ul li {
+            display: inline;
+            margin-right: 20px;
+        }
+        img {
+            margin: 10px;
+        }
